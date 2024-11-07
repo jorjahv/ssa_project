9ed3b83dfce15cd6d34a4d621a88f1da3f8b4d65
Jorjah Vunich <jorjahvunich@gmail.com>
2024-11-07 15:05:53 +1100

    Refactor Invitation Email Notification

--- chipin/urls.py
+++ chipin/urls.py
@@ -4,3 +4,15 @@ from . import views
 urlpatterns = [
    path("", views.home, name="home"),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path("", views.home, name="home"),
+   path('create_group/', views.create_group, name='create_group'),
+   path('group//', views.group_detail, name='group_detail'),
+   path('group//invite/', views.invite_users, name='invite_users'),
+   path('group//delete/', views.delete_group, name='delete_group'),
+   path('accept-invite/<int:group_id>/', views.accept_invite, name='accept_invite'),
+]
